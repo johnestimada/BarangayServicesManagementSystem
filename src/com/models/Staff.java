36dd0da5000880491db1e913f6edc157e653e4dd
PROGLAB-29 <ryel.reyes.college@ustangelicum.edu.ph>
2026-06-25 13:46:48 +0800

    Staff.java
    Put Staff class to represent barangay staff.

---
+++ src/com/models/Staff.java
@@ -0,0 +1,8 @@
+
+package com.barangay.models;
+
+public class Staff {
+    protected String name;
+    public Staff(String name) { this.name = name; }
+    public String getName() { return name; }
+}
