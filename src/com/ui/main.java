13b9743925566bb876c15ac92e7efc889fed91ef
PROGLAB-31 <john.estimada.college@ustangelicum.edu.ph>
2026-06-25 13:57:26 +0800

    Main.java update
    added conditions
    added registration, document request status, clearance request and status.

--- src/com/ui/main.java
+++ src/com/ui/main.java
@@ -14,6 +14,27 @@ public class Main {
 
         MenuNavigator.showMainMenu();
         int choice = sc.nextInt(); 
+
+         switch (choice) {
+            case 1 -> {
+                Resident r = new Resident("Juan", 25);
+                residentService.registerResident(r);
+                System.out.println("Registered: " + r);
+            }
+            case 2 -> {
+                DocumentRequest d = new DocumentRequest();
+                dispatcher.addRequest(d);
+                d.process();
+                System.out.println("Document status: " + d.getStatus());
+            }
+            case 3 -> {
+                ClearanceRequest c = new ClearanceRequest();
+                dispatcher.addRequest(c);
+                c.process();
+                System.out.println("Clearance status: " + c.getStatus());
+            }
+            default -> System.out.println("Invalid choice");
+        }
     
     }
 }
