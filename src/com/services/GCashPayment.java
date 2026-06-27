a85682480a866380a7cf68164ea8bb2d06d527c0
MMLAB-14 <john.rabino.college@ustangelicum.edu.ph>
2026-06-27 15:59:59 +0800

    added GCash payment
    GCash payment implementation

--- src/com/services/GCashPayment.java
+++ src/com/services/GCashPayment.java
@@ -1,4 +1,11 @@
-public class GCash{
+package com.barangay.services;
 
-    
+public class GCashPayment implements Payment {
+    @Override
+    public void pay(double amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Invalid payment amount");
+        }
+        System.out.println("GCash payment of " + amount + " processed.");
+    }
 }
