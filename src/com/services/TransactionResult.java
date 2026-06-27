f3fde998f26fa74a900600d787cdce10065945b1
MMLAB-13 <john.estimada.college@ustangelicum.edu.ph>
2026-06-27 15:40:18 +0800

    constructor and getters added

--- src/com/services/TransactionResult.java
+++ src/com/services/TransactionResult.java
@@ -5,4 +5,11 @@ public class TransactionResult<T> {
     private T data;
     private String message;
 
+    public TransactionResult(boolean success, T data, String message) {
+        this.success = success;
+        this.data = data;
+        this.message = message;
+    }
+
 }
+
