83c17424c55e0abb37c130f014ffe8d0d56aa20f
COLLEGELAB-08 <john.rabino.college@ustangelicum.edu.ph>
2026-06-27 09:07:38 +0800

    Create MenuNavigator
    Create MenuNavigator class fot diplaying menu options

---
+++ src/com/ui/MenuNaviagator.java
@@ -0,0 +1,9 @@
+package com.barangay.ui;
+
+public  class MenuNavigator {
+    public static void ShowMainMenu(){
+        System.out.println("1. Register as a Resident");
+        System.out.println("2. Request the Document");
+        System.out.println("3. Request the Clearance")
+    }
+}
