fdaf8ab0d0edfd1b555494467096ace5b66ac395
COLLEGELAB-08 <john.rabino.college@ustangelicum.edu.ph>
2026-06-27 09:51:11 +0800

    Implementation of  payment
    with GCash integration and added other files

--- src/com/ui/MenuNaviagator.java
+++ src/com/ui/MenuNaviagator.java
@@ -2,8 +2,8 @@ package com.barangay.ui;
 
 public  class MenuNavigator {
     public static void ShowMainMenu(){
-        System.out.println("1. Register as a Resident");
-        System.out.println("2. Request the Document");
-        System.out.println("3. Request the Clearance")
+        System.out.println("1. Register a Resident");
+        System.out.println("2. Request a Document");
+        System.out.println("3. Request a Clearance")
     }
 }
